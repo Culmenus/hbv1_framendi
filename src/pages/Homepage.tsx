2580fb6ce1e96c11b886436890e9bb58fca48724
dha7 <dha7@hi.is>
2021-11-18 19:41:58 +0000

    also homapage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,14 +1,14 @@
 // veit ikki á þetta kannski að vera Index.tsx?
 import logo from '../logo.svg';
 
-function Homepage() {
-  return (<div>I am homepage boiii
+const Homepage: React.FC = () => {
+  return (
+  <div>I am homepage boiii let's get some forums up in hyah
     <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. Factual factual.
         </p>
-  </div>
-  
+  </div>  
     );
 }
 
