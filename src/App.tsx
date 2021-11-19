5bbc4b41444b1a4615bf5028fdbaa6f866097bc1
unknown <natidemisket@gmail.com>
2021-11-19 23:35:29 +0000

    signup ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,21 @@ import Footer from './components/footer/Footer';
 import NotFound from './components/notfound/NotFound';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/LoginPage/Login';
-
 import './App.css';
+import SignUp from './pages/SignupPage/Signup';
 
 const App: React.FC = () => {
   return (
     <>
-      <Header/>
+      {/*<Header/> */}
       <Routes>
         <Route path="/" element = {<Homepage/>}/>
         <Route path="/forum/:id" element = {<Forum/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path='/signup' element={<SignUp/>} />
         <Route element = {<NotFound/>}/>
       </Routes>
-      <Footer/>
+      {/*<Footer/>*/}
     </>
   );
 }
