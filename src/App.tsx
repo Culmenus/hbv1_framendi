8de3d5957a2a6f1afcd11bb8a7850750f641aab6
natidemis <natidemisket@gmail.com>
2021-11-19 20:24:42 +0000

    path renames

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Forum from './components/forum/Forum'
-import Homepage from './pages/Homepage/Homepage';
+import Homepage from './pages/HomePage/Homepage';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import NotFound from './components/notfound/NotFound';
