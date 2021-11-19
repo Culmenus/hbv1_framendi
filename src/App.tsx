417ffd591d686a4521e34eccc9067505d50541ab
Daníel Helgi Ágústsson <danielhelgi1@gmail.com>
2021-11-19 19:26:43 +0000

    broken dreams

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import React from 'react';
 import Forum from './components/forum/Forum'
 import Homepage from './pages/Homepage';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import NotFound from './components/notfound/NotFound';
+import { Switch, Route } from 'react-router';
+
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <React.Fragment>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={Homepage}/>
+        <Route exact path="/forum/:id" component={Forum}/>
+        <Route component={NotFound}/>
+      </Switch>
+      <Footer/>
+    </React.Fragment>
+    
   );
 }
 
---
+++ src/components/notfound/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound: React.FC = () => {
+  return (
+    <div>
+      404 boiiii
+    </div>
+  );
+}
+
+export default NotFound;
