f36087320c66c036db1b596e625188ef5418fd9f
natidemis <natidemisket@gmail.com>
2021-11-19 19:47:06 +0000

    sma refactoring a 'pages'

---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,15 @@
+// veit ikki á þetta kannski að vera Index.tsx?
+import logo from '../logo.svg';
+
+const Homepage: React.FC = () => {
+  return (
+  <div>I am homepage boiii let's get some forums up in hyah
+    <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload. Factual factual.
+        </p>
+  </div>  
+    );
+}
+
+export default Homepage;
