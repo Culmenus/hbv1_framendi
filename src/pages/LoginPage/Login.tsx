48701e7ae15f9c291ea1e2bcb73d154c34640619
natidemis <natidemisket@gmail.com>
2021-11-19 21:53:04 +0000

    login twerk

--- src/pages/LoginPage/Login.tsx
+++ src/pages/LoginPage/Login.tsx
@@ -1,1 +1,18 @@
-export {}
+import React from "react";
+import {StyledContainerDiv, StyledMainDiv, StyledTextInput, StyledForm} from './LoginStyles';
+
+export default function Login() {
+    return (
+        <>
+            <StyledContainerDiv>
+                <StyledMainDiv>
+                    {/* How dis work again?*/}
+                    <StyledForm action="/login" method="post">
+                        <StyledTextInput type="text" name="username" placeholder="username"/>
+                        <StyledTextInput type="password" name="password" placeholder="password"/>
+                    </StyledForm>
+                </StyledMainDiv>
+            </StyledContainerDiv>
+        </>
+    )
+}
---
+++ src/pages/LoginPage/LoginStyles.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components'
+
+
+export const StyledContainerDiv = styled.div`
+    margin-top: 12%;
+    clear: both;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: black;
+    overflow: auto;
+`;
+
+
+export const StyledMainDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 274px;
+    height: 225px;
+    background: rgba(255,255,255, 0.3);
+    mix-blend-mode: normal;
+
+`;
+
+export const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color:
+`;
+
+export const StyledTextInput = styled.input`
+    width: 100%;
+    height: 39px;
+    background: #FFFFFF;
+    border: none;
+    margin-bottom: 5%;
+    text-align: center;
+    font-family: Raleway;
+    font-size: 15px;
+`;
