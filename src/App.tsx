fcd76bc6ca153ba0d527dc00aa48150ea5b9a32e
jongvnnar <jgh060@gmail.com>
2021-11-19 23:30:33 +0000

    idk why

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import Forum from './components/forum/Forum'
-import Homepage from './pages/HomePage/Homepage';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import NotFound from './components/notfound/NotFound';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import Forum from "./components/forum/Forum";
+import Homepage from "./pages/Homepage/Homepage";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import NotFound from "./components/notfound/NotFound";
+import { Route, Routes } from "react-router-dom";
 
-
-import './App.css';
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element = {<Homepage/>}/>
-        <Route path="/forum/:id" element = {<Forum/>}/>
-        <Route path="/asdf" element = {<Forum/>}/>
-        <Route element = {<NotFound/>}/>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/forum/:id" element={<Forum />} />
+        <Route path="/asdf" element={<Forum />} />
+        <Route element={<NotFound />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
-}
+};
 
 export default App;
--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,15 +1,15 @@
 // veit ikki á þetta kannski að vera Index.tsx?
-import React from 'react';
+import React from "react";
 
 const Homepage: React.FC = () => {
   return (
-  <React.Fragment>
+    <React.Fragment>
       I am homepage boiii let's get some forums up in hyah
-    <p>
-      Edit <code>src/App.tsx</code> and save to reload. Factual factual.
-    </p>
-  </React.Fragment>  
-    );
-}
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload. Factual factual.
+      </p>
+    </React.Fragment>
+  );
+};
 
 export default Homepage;
