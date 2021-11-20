ecd4b71066fbe5867229d2fd34e77b3050620a06
jongvnnar <jgh060@gmail.com>
2021-11-20 00:59:21 +0000

    overview card ui

---
+++ src/components/forumView/forumView.tsx
@@ -0,0 +1,34 @@
+import { Card, CardMedia, CardContent, Typography, Link } from "@mui/material";
+import { Forum } from "../../types/Forum";
+type Props = {
+  forum: Forum;
+};
+export function ForumView({ forum }: Props) {
+  const imgNum = Math.floor(Math.random() * 17) + 1;
+  const img = require(`./cards/img-${imgNum}.jpg`);
+  console.log(img);
+  return (
+    <Link href={`forums/${forum.id}`} underline="hover">
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={img.default}
+          alt="picture for forum"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {forum.name}
+          </Typography>
+          <Typography
+            style={{ wordWrap: "break-word" }}
+            variant="body2"
+            color="text.secondary"
+          >
+            {forum.description}
+          </Typography>
+        </CardContent>
+      </Card>
+    </Link>
+  );
+}
