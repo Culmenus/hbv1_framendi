2385ef47c1e4d97c44be7a459c19cd6397126cde
jongvnnar <jgh060@gmail.com>
2021-11-23 21:56:19 +0000

    fetch forums

--- src/components/thread/Thread.tsx
+++ src/components/thread/Thread.tsx
@@ -12,8 +12,8 @@ import PersonIcon from "@mui/icons-material/Person";
 import AddIcon from "@mui/icons-material/Add";
 import Typography from "@mui/material/Typography";
 import { blue } from "@mui/material/colors";
-import {Chat} from './chat';
-import {Thread as TThread} from '../../types/Thread';
+import { Chat } from "./chat";
+import { Thread as TThread } from "../../types/Thread";
 import { useState } from "react";
 import { makeStyles } from "@material-ui/styles";
 import { Box, Container, createStyles, TextField } from "@mui/material";
@@ -26,28 +26,31 @@ const useStyles = makeStyles(() => ({
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
-   },
+  },
 }));
-export default function ThreadComponent({thread}:{thread: TThread | null}) {
+export default function ThreadComponent({
+  thread,
+}: {
+  thread: TThread | null;
+}) {
   const classes = useStyles();
   const [messages, setMessages] = useState<Array<Message>>([]);
   const [value, setValue] = useState<string>("");
-  console.log(thread)
-    return (
-      <Box
+  console.log(thread);
+  return (
+    <Box
       display="flex"
       alignItems="center"
       flexDirection="column"
       width="100%"
       height="100%"
-      maxHeight="80vh"
+      maxHeight="75vh"
     >
       <Container
         style={{
           overflow: "auto",
           display: "flex",
           flexDirection: "column-reverse",
-          height: "81vh",
           paddingBottom: "10px",
           marginTop: "auto",
           alignContent: "flex-end",
@@ -72,7 +75,5 @@ export default function ThreadComponent({thread}:{thread: TThread | null}) {
         }}
       />
     </Box>
-    );
-
+  );
 }
-
