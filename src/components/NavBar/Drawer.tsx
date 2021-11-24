b72efe420f0e7979b51e27763492e0d1f00c3cc5
natidemis <natidemisket@gmail.com>
2021-11-24 15:54:27 +0000

    stæla edit/delete á thread

--- src/components/NavBar/Drawer.tsx
+++ src/components/NavBar/Drawer.tsx
@@ -10,7 +10,7 @@ import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { Box, Link, Toolbar, Typography } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
-import { ThemeToggleButton } from "./NavBar";
+import { ThemeToggleButton } from "./ThemeButton";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import { deepPurple } from "@mui/material/colors";
